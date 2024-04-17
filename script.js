52abbfd63ef504a4f1975212bce952d8a965b534
Keshav Madhav <keshav2552003@gmail.com>
2024-04-17 09:28:47 +0530

    Added canvas resizing to 16:9 ratio

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+//resize canvas
+window.addEventListener('resize', resizeCanvas);
+function resizeCanvas() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  
+  // Check if height exceeds the 16:9 ratio
+  if(height > width * 0.5625) {
+    height = width * 0.5625;
+  } else {
+    width = height * 1.77777778;
+  }
+
+  height = Math.floor(height/64) * 64;
+  width = Math.floor(width/64) * 64;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+resizeCanvas();
